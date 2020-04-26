2a77b1f199617e458026430b596e56a260ef140f
LabMem010 <38588297+LabMem010@users.noreply.github.com>
2020-04-26 20:46:34 +0800

    Create 6-2.c

---
+++ 基础编程题目集/函数题/6-2.c
@@ -0,0 +1,30 @@
+// 6-2 多项式求值
+#include <stdio.h>
+
+#define MAXN 10
+
+double f(int n, double a[], double x);
+
+int main()
+{
+    int n, i;
+    double a[MAXN], x;
+
+    scanf("%d %lf", &n, &x);
+    for (i = 0; i <= n; i++)
+        scanf("%lf", &a[i]);
+    printf("%.1f\n", f(n, a, x));
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+double f(int n, double a[], double x)
+{
+    int i;
+    double p = a[0];
+    for (i = 1; i <= n; i++)
+    {
+        p += (a[i] * pow(x, i));
+    }
+    return p;
+}
