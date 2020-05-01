4dab43d5e01996ba70d72aa19c666ae54bd57c38
LabMem010 <38588297+LabMem010@users.noreply.github.com>
2020-05-01 10:07:39 +0800

    Update 6-11.c

--- 基础编程题目集/函数题/6-11.c
+++ 基础编程题目集/函数题/6-11.c
@@ -23,18 +23,19 @@ int main()
 }
 
 /* 你的代码将被嵌在这里 */
+
 ElementType Median(ElementType A[], int N)
 {
     ElementType temp;
-    for (int i = 0; i <= N; i++)
+    for (int gap = N / 2; gap > 0; gap = gap / 2) //gap是每次排序分组的间隔，每次间隔缩小两倍（其他缩小办法也可以）
     {
-        for (int j = 1; j < N; j++)
+        for (int i = gap; i < N; i++) //相当于在同一组内采用直接插入排序
         {
-            if (A[j] < A[i])
+            for (int j = i - gap; j >= 0 && A[j] > A[j + gap]; j = j - gap) //如果同一组内前一个元素大于相 gap个位置的元素，则两者交换位置
             {
-                A[i] = temp;
-                A[i] = A[j];
-                A[j] = temp;
+                temp = A[j];
+                A[j] = A[j + gap];
+                A[j + gap] = temp;
             }
         }
     }
